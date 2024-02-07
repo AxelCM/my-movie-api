200a33a877f022def167b2cc63d4425a5a569dfe
AxelCM <axel-cas@hotmail.com>
2024-02-07 14:27:00 -0600

    update props of basemodel

--- entities/movies.py
+++ entities/movies.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Movie(BaseModel):
     id: int
-    name: str
-    year: int
-    director: str
-    category: str
+    name: str = Field(max_length=100, min_length=2, title="Movie Name", description="Name of the movie", example="The Godfather")
+    year: int = Field(gt=1900, lt=2022, title="Release Year", description="Year the movie was released", example=1972)
+    director: str = Field(max_length=100, min_length=2, title="Director", description="Name of the director", example="Francis Ford Coppola")
+    category: str = Field(max_length=100, min_length=2, title="Category", description="Category of the movie", example="Crime")
